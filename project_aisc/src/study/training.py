d5af8f4fd6c0d990f7bbf1b8f93965387ac8d628
Claudio <claudio.pereti@outlook.com>
2021-04-29 02:26:45 +0200

    backbone of phi on tc

--- project_aisc/src/study/training.py
+++ project_aisc/src/study/training.py
@@ -22,8 +22,8 @@ atom_data = Processing.DataProcessor(ptable, sc_dataframe)
 
 
 path = '../../data/processed/'
-atom_data.load_data_processed(path + 'dataset_elaborated.csv')
-atom_data.load_data_processed(path + 'dataset_label_elaborated.csv')
+atom_data.load_data_processed(path + 'dataset_complete.csv')
+atom_data.load_data_processed(path + 'dataset_complete_label.csv')
 atom_data.build_Atom()
 atom_data.build_dataset()
 
@@ -33,14 +33,61 @@ X,X_test,Y,Y_test = atom_data.train_test_split(X,Y,test_size = 0.2)
 #%%
 #Build and train the deep set model
 
-model = DeepSet(DataProcessor=atom_data,latent_dim = 1)
+model = DeepSet(DataProcessor=atom_data,latent_dim = 1,freeze_latent_dim_on_tuner =True)
 # import importlib
-# importlib.reload(DeepSets)
+# importlib.reload(Processing)
 
+model.load_best_model(directory='../../models/best_model_26-04/',project_name='model_26-04-0')
+#%%
+
+mat = 'H2'
+mat = 'Al14.9Mg44.1Zn41.0'
+
+
+quasi_crystall = atom_data.get_input(mat)
+model.rho.layers[10].predict(quasi_crystall)
+model.rho.predict(quasi_crystall)
+model_phi = model.rho.layers[10]
+model_phi.summary()
+(model.rho.layers[11])
+model_phi.predict(quasi_crystall)
+model.rho.layers[10](quasi_crystall)
+model.rho.layers[11]
+from tensorflow.keras import backend as K
+inputs = model.rho.layers[:10]
+mod = model.rho.layers[10]
+
+from tensorflow.keras.layers import Input,Add
+from tensorflow.keras.models import Model
+layers[0].output
+phi = Model(inputs =inputs ,outputs =outputs )
+inputs = [Input(33) for i in range(10)]
+out = [mod(i) for i in inputs]
+outputs = Add()(out)
+phi.predict(X_test)[0][0]
+#%%
+from mendeleev import element
+tav_per = {}
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
+
+for i in range(1,97):
+    el = element(i).symbol
+    el = el
+    el_input = atom_data.get_input(el)
+    tav_per[el] = [phi.predict(el_input)[0][0]]
+tav_per
+tav_x_phi = pd.DataFrame.from_dict(tav_per)
+tav_x_phi.to_csv()
+phi.compile(optimizer = Adam(1e-5),loss = 'mse',metrics =['mae'])
+phi.fit(X,Y,epochs=10,validation_data=(X_val,Y_val),callbacks = [EarlyStopping(monitor = 'val_loss', min_delta = 0.05,patience = 5, restore_best_weights = True)])
+np.sqrt(phi.evaluate(X_test,Y_test)[0])
+model.rho.evaluate(X_test,Y_test)
+#%%
 
 model.build_model()
 model.phi.summary()
-model.rho.summary()
+model.rho.layers[10].summary()
 
 callbacks = []
 model.fit_model(X,Y,X_val,Y_val,callbacks= callbacks)
@@ -48,19 +95,26 @@ model.evaluate_model(X_test,Y_test)
 model.visual_model_perform()
 path_to_save = '../../models/'
 model.save_model(path_to_save,'model0')
-model.load_best_model(directory = '../../models/best_model_11-04/',project_name ='model_11-04-3')
+model.load_best_model(directory = '../../models/best_model_16-04/',project_name ='model_16-04-0')
 model.load_model(path='../../models',name='/')
 model.load_best_architecture(directory = '../../models/best_model_11-04/',project_name ='model_11-04-3')
 model.rho.layers[10].predict(X_test)
 model.rho.layers[10].summary()
 
-
+phi.save(path_to_save + 'phi_model')
 #display and save the prediction vs the observed value or the critical Temperature
 
-observed_vs_predicted = pd.DataFrame({'Oberved Critical Temperature (K)':Y_test,'Predicted Critical Temperature (K)':np.array(model.rho.predict(X_test)).reshape(Y_test.shape[0],)})
-
+observed_vs_predicted = pd.DataFrame({'Oberved Critical Temperature (K)':Y_test,'Predicted Critical Temperature (K)':np.array(phi.predict(X_test)).reshape(Y_test.shape[0],)})
+#%%
 sns_plot = sns.scatterplot(x = observed_vs_predicted['Oberved Critical Temperature (K)'],y= observed_vs_predicted['Predicted Critical Temperature (K)']).get_figure()
+# plt.scatter(x=0.05,y=model.rho.predict(quasi_crystall),color = 'r')
+# plt.scatter(x=0.8,y=2.23,color = 'y')
+# plt.xlim([0,5])
+# plt.ylim([0,5])
+plt.savefig('predicted_vs_observed_atom.png')
 
+
+#%%
 sns_plot.savefig("training_img/pred_vs_ob.png")
 #%%
 for i in range(10):
@@ -86,8 +140,9 @@ mono_rapp = model.rho.layers[10].predict(list(atom_data.dataset))
 mono_temp = model.rho.predict(list(atom_data.dataset))
 mono_temp.shape
 mono_rapp.shape
+mono_temp[:]
 #changed t_pred con mono_rapp
-mono_dataset = pd.DataFrame.from_dict({'x':np.reshape(mono_rapp[:,1],(mono_rapp.shape[0])),'temp_pred':np.reshape(mono_rapp[:,3],(mono_rapp.shape[0])),'temp_oss': atom_data.t_c})
+mono_dataset = pd.DataFrame.from_dict({'x':np.reshape(mono_rapp[:,0],(mono_rapp.shape[0])),'temp_pred':np.reshape(mono_temp[:,0],(mono_rapp.shape[0])),'temp_oss': atom_data.t_c})
 mono_dataset.to_csv('mono_dim_data/mono_dim_rapp.csv')
 mono_dataset = pd.read_csv('mono_dim_data/mono_dim_rapp.csv',index_col=0)
 mono_dataset.head()
@@ -155,5 +210,5 @@ def plot_fig(x,y,color='ro',save = False,path= None,name = None,xlabel=None,ylab
     plt.ylabel(ylabel)
     if save:
         plt.savefig(path+name)
-    # plt.xlim([0.4,1])
+    plt.xlim([-0.2,0.2])
     plt.show()
