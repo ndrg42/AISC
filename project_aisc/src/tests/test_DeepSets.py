5eb100564eb06f19651109e7e253ed1197f2b33a
Claudio <claudio.pereti@outlook.com>
2022-01-05 14:12:13 +0100

    test for Deep Sets fitting

--- project_aisc/src/tests/test_DeepSets.py
+++ project_aisc/src/tests/test_DeepSets.py
@@ -7,7 +7,7 @@ import pytest
 import pandas as pd
 import numpy as np
 import chela
-
+from sklearn.model_selection import train_test_split
 import DataLoader
 import Processing
 import DeepSets
@@ -18,13 +18,31 @@ importlib.reload(DeepSets)
 def supercon_dataset():
     return list(np.load('data/processed/dataset_supercon.npy'))
 
+@pytest.fixture
+def supercon_temperature():
+    return DataLoader.SuperCon(sc_path ='data/raw/supercon_tot.csv')['critical_temp']
+
 
 def test_regressor_deepset_build(supercon_dataset):
-    """Check if model definitions work"""
+    """Check if model definition work"""
     regressor_deepset = DeepSets.RegressorDeepSet(supercon_dataset)
     regressor_deepset.build_model()
 
     assert regressor_deepset.rho != None and regressor_deepset.phi != None
+
+def test_deepset_train(supercon_dataset,supercon_temperature):
+    """Test method for model fitting"""
+
+    X,X_val,y,y_val = train_test_split(np.moveaxis(np.array(supercon_dataset),0,1),np.array(supercon_temperature),test_size = 0.3)
+    X  = list(np.moveaxis(X,0,1))
+    X_val  = list(np.moveaxis(X_val,0,1))
+
+    regressor_deepset = DeepSets.RegressorDeepSet(supercon_dataset)
+    regressor_deepset.build_model()
+    regressor_deepset.fit_model(X=X,y=y,X_val=X_val,y_val=y_val,epochs=1)
+
+    assert regressor_deepset.history != None
+
 
 #
 # sc_dataframe = DataLoader.SuperCon(sc_path ='data/raw/supercon_tot.csv')
