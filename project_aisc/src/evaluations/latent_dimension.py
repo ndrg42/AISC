5bae9a518c83c23f6bd4a2a2c0f23bca246cbf34
Claudio <claudio.pereti@outlook.com>
2022-05-07 17:10:06 +0200

    added parser for model and config sleection

--- project_aisc/src/evaluations/latent_dimension.py
+++ project_aisc/src/evaluations/latent_dimension.py
@@ -10,17 +10,50 @@ import yaml
 from yaml import Loader
 import importlib
 importlib.reload(build_models)
-import os
-os.getcwd()
-os.chdir('/home/claudio/AISC/project_aisc/')
 import seaborn as sns
 import numpy as np
 from mendeleev import element
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
+import os
+
+def latent_parser():
+
+    with open('/home/claudio/AISC/project_aisc/config/avaible_model_config.yaml') as file:
+        model_config = yaml.load(file,Loader)
+
+
+    my_parser = argparse.ArgumentParser(prog='latent_dimension',
+                                        description = "train and display latent space of linear model",
+                                        usage = '%(prog)s [options]',
+                                        )
+    my_parser.add_argument('-model',
+                          action = 'store',
+                          nargs = '+',
+                          metavar = ('MODEL'),
+                          help = """Select the linear model. Possible choice:
+                                  -linear regressor
+                                  -linear classifier"""
+    )
+
+    my_parser.add_argument('-config',
+                          action = 'store',
+                          nargs = 1,
+                          metavar = ('CONFIG'),
+                          help = """Use a custom config for the ML model.
+                                   The model need to be specified"""
+    )
+
+    #Parse the args
+    args = my_parser.parse_args()
+
+    return args
 
 
 def main():
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
     with open('/home/claudio/AISC/project_aisc/config/latent_dimension_config.yaml') as file:
         model_config = yaml.load(file,Loader)
@@ -39,19 +72,45 @@ def main():
     #Process SuperCon data
     supercon_processed = supercon_processor.get_dataset()
 
-    tc_regression = sc_dataframe['critical_temp']
-    X,X_test,Y,Y_test = build_features.train_test_split(supercon_processed, tc_regression, 0.2)
+    len_argv = len(sys.argv)
+    model_config_path = None
+
+    if len_argv > 1:
+        args = latent_parser()
+        model_name = ' '.join(args.model)
+        model_config_path = args.config
+
+    else:
+        model_name = 'linear regressor'
+
+    if 'regressor' in model_name:
+        tc = sc_dataframe['critical_temp']
+        #Select material with high temperature (tc > 10 K)
+        mask_temperature_materials = np.where(tc > 10,1,0)
+        legend_latent_space = ['High temperature','Low temperature']
+
+    elif 'classifier' in model_name:
+        tc = np.where(sc_dataframe['critical_temp']>0,1,0)
+        mask_temperature_materials = tc
+        legend_latent_space = ['Superconductor','Non Superconductor']
+
+    if model_config_path is not None:
+        model_config_path = model_config_path[0]
+
+        with open(model_config_path) as file:
+            model_config = yaml.load(file,Loader)
+
+
+    X,X_test,Y,Y_test = build_features.train_test_split(supercon_processed, tc, 0.2)
     X,X_val,Y,Y_val = build_features.train_test_split(X, Y, 0.2)
 
-    build_models.model_config_initializer(model_config, model_name='linear regressor')
     #Define model and train it
-    model = build_models.get_model(model_name = 'linear regressor',model_config = model_config)
+    model = build_models.get_model(model_name = model_name ,model_config = model_config)
     callbacks = [tf.keras.callbacks.EarlyStopping(min_delta = 5, patience = 40, restore_best_weights = True)]
     model.fit(X, Y, validation_data = (X_val,Y_val), epochs = 1, callbacks = callbacks)
 
-    materials_representation = model.material_representation(supercon_processed)
 
-    high_temperature_materials = np.where(tc_regression > 10,1,0)
+    materials_representation = model.material_representation(supercon_processed)
 
     elements = [element(i).symbol for i in range(1,97)]
 
@@ -62,10 +121,10 @@ def main():
 
     fig,axs = plt.subplots(1,2, figsize=(11,8))
 
-    sns.histplot(x = np.reshape(materials_representation.numpy(),(-1,)), hue = high_temperature_materials, ax = axs[0])
+    sns.histplot(x = np.reshape(materials_representation.numpy(),(-1,)), hue = mask_temperature_materials, ax = axs[0])
     axs[0].set_title('Material Latent Representation')
     axs[0].set_xlabel('Latent feature')
-    axs[0].legend(['High temperature','Low temperature'])
+    axs[0].legend(legend_latent_space)
 
     sns.scatterplot(data = pd.DataFrame(model.atom_representation(atoms_processed).numpy(),index=range(1,97)), ax = axs[1])
     axs[1].set_title('Atomic Latent Representation')
