0c874b9095474cf9f8bd7fa5f1ca8108d1d73f6a
Claudio <claudio.pereti@outlook.com>
2022-08-15 18:52:51 +0200

    refactored preprocessing file: can customize regression or classification problem

--- project_aisc/src/features/preprocessing.py
+++ project_aisc/src/features/preprocessing.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from data import make_dataset
 from features import build_features
 import yaml
@@ -6,7 +8,7 @@ import pathlib
 import numpy as np
 
 
-def preprocess_data(problem, supercon_path, garbagein_path, seed):
+def preprocess_data(problem, supercon_data, garbagein_data, test_split, val_split, seed):
     # Load atomic data
     ptable = make_dataset.get_periodictable()
     # Initialize the processor for atomic data
@@ -15,17 +17,24 @@ def preprocess_data(problem, supercon_path, garbagein_path, seed):
     atom_processed = atom_processor.get_atom_data()
 
     if problem == 'regression':
-        sc_dataframe = make_dataset.get_supercon(name='supercon.csv')
+        sc_dataframe = make_dataset.get_supercon(name=supercon_data)
         tc = sc_dataframe['critical_temp']
+    elif problem == 'classification':
+        supercon_dataframe = make_dataset.get_supercon(name=supercon_data)
+        garbagein_dataframe = make_dataset.get_supercon(name=garbagein_data)
+        tc_supercon = supercon_dataframe['critical_temp']
+        tc_garbage = garbagein_dataframe['critical_temp']
+        # Merge supercondutors data non-superconductors data
+        sc_dataframe = pd.concat([supercon_dataframe, garbagein_data], ignore_index=True)
+        tc = pd.concat([tc_supercon,tc_garbage], ignore_index=True)
 
     # Initialize processor for SuperCon data
     supercon_processor = build_features.SuperConData(atom_processed, sc_dataframe)
     # Process SuperCon data
     supercon_processed = supercon_processor.get_dataset()
 
-    X, X_test, Y, Y_test = build_features.train_test_split(supercon_processed, tc,
-                                                           model_config['train setup']['test split'])
-    X, X_val, Y, Y_val = build_features.train_test_split(X, Y, model_config['train setup']['validation split'])
+    X, X_test, Y, Y_test = build_features.train_test_split(supercon_processed, tc, test_split, seed)
+    X, X_val, Y, Y_val = build_features.train_test_split(X, Y, val_split, seed)
 
     np.save(str(pathlib.Path(__file__).absolute().parent.parent.parent) + '/data/processed/train/X_train.npy',
             np.array(X))
@@ -48,6 +57,4 @@ if __name__ == '__main__':
     with open(preprocessing_config_path) as file:
         preprocessing_config = yaml.load(file, Loader)
 
-    print(preprocessing_config_path)
-    print(preprocessing_config)
-    #preprocess_data()
+    preprocess_data(**preprocessing_config)
