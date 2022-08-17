5a112b69640b79ab481e859d97a6fb7bd3ed9cbb
Claudio <claudio.pereti@outlook.com>
2022-08-17 16:07:56 +0200

    added parser for classification/regression problem and refactored code

--- project_aisc/src/laboratory/evaluate.py
+++ project_aisc/src/laboratory/evaluate.py
@@ -3,7 +3,48 @@ import numpy as np
 import pathlib
 import tensorflow as tf
 import json
-import pandas as pd
+import argparse
+import sys
+import warnings
+
+def evaluate_parser():
+    my_parser = argparse.ArgumentParser(prog='evaluate model',
+                                        description="evaluate model on superconductivity data",
+                                        usage='%(prog)s [options]',
+                                        )
+
+    my_parser.add_argument('-problem',
+                           action='store',
+                           nargs=1,
+                           metavar='CONFIG',
+                           choices=['regression', 'classification'],
+                           help="""Set the type of problem, classification or regression"""
+                           )
+
+    my_parser.add_argument('--no-save',
+                           action='store',
+                           nargs='?',
+                           help="Don't save/track results with mlflow. Arguments specify what to not track",
+                           choices=['model', 'all'],
+                           const='all'
+                           )
+
+    # Parse the args
+    args = my_parser.parse_args()
+
+    return args
+
+
+def get_metrics(Y_pred, Y_true, problem='classification'):
+    if problem == 'regression':
+        mae = tf.keras.metrics.MeanAbsoluteError()(Y_pred, Y_true)
+        mse = tf.keras.metrics.MeanSquaredError()(Y_pred, Y_true)
+        return {'mae': float(mae), 'mse': float(mse), 'rmse': float(tf.math.sqrt(mse))}
+    elif problem == 'classification':
+        threshold = 0.5
+        precision = tf.keras.metrics.Precision(thresholds=threshold)(Y_true, Y_pred)
+        recall = tf.keras.metrics.Recall(thresholds=threshold)(Y_true, Y_pred)
+        return {'precision': float(precision), 'recall': float(recall)}
 
 
 def main():
@@ -17,37 +58,46 @@ def main():
     model_uri = home_path + "/data/experiments/mlruns/" + experiment_id + "/" + run_id + "/artifacts/model"
     model = mlflow.keras.load_model(model_uri)
 
+    len_argv = len(sys.argv)
+    if len_argv > 1:
+        args = evaluate_parser()
+        if args.problem is not None:
+            problem = args.problem[0]
+        else:
+            problem = 'regression'
+            warnings.warn("Problem not specified! Default problem is regression")
+    else:
+        problem = 'regression'
+        warnings.warn("Problem not specified! Default problem is regression")
+
     X_test = list(np.load(home_path + '/data/processed/test/X_test.npy'))
     Y_test = np.load(home_path + '/data/processed/test/Y_test.npy')
 
-    Y_pred = tf.reshape(model.predict(X_test), shape=(-1,))
-    mae = tf.keras.metrics.mean_absolute_error(Y_pred, Y_test)
-    mse = tf.keras.metrics.mean_squared_error(Y_pred, Y_test)
-    print(f"MAE : {mae}\nMSE : {mse}\nRMSE : {tf.math.sqrt(mse)}")
+    Y_pred = model.predict(X_test)
+    metrics = get_metrics(Y_pred=Y_pred, Y_true=Y_test, problem=problem)
+    print("Evaluation on Test set:")
+    [print(f"{item[0]} : {item[1]}") for item in metrics.items()]
 
     MlflowClient = mlflow.tracking.client.MlflowClient(tracking_uri=home_path + "/data/experiments/mlruns/")
 
-    MlflowClient.log_metric(run_id=run_id, key='test_root_mean_squared_error', value=float(mae))
-    MlflowClient.log_metric(run_id=run_id, key='test_mean_squared_error', value=float(mse))
-    MlflowClient.log_metric(run_id=run_id, key='test_mean_absolute_error', value=float(tf.math.sqrt(mse)))
+    [MlflowClient.log_metric(run_id=run_id, key='test_' + metric, value=value) for metric, value in metrics.items()]
 
     artifact_uri = home_path + "/data/experiments/mlruns/" + experiment_id + "/" + run_id + "/artifacts"
     # Save predictions of the model
     np.save(artifact_uri + "/predictions.npy", tf.reshape(model.predict(X_test), shape=(-1,)).numpy())
 
-    X_hosono = list(np.load(home_path + "/data/processed/hosono_dataset.npy"))
-    Y_hosono = pd.read_csv(home_path + '/data/external/hosono_ready.csv')['critical_temp']
+    X_hosono = list(np.load(home_path + "/data/processed/hosono.npy"))
+    Y_hosono = np.load(home_path + '/data/processed/Y_hosono.npy')
 
-    Y_hosono_pred = tf.reshape(model.predict(X_hosono), shape=(-1,))
-    mae = tf.keras.metrics.mean_absolute_error(Y_hosono_pred, Y_hosono)
-    mse = tf.keras.metrics.mean_squared_error(Y_hosono_pred, Y_hosono)
-    print(f"MAE : {mae}\nMSE : {mse}\nRMSE : {tf.math.sqrt(mse)}")
+    Y_hosono_pred = model.predict(X_hosono)
+    metrics = get_metrics(Y_pred=Y_hosono_pred, Y_true=Y_hosono, problem=problem)
+    print("Evaluation on Hosono:")
+    [print(f"{item[0]} : {item[1]}") for item in metrics.items()]
 
     X_ima = list(np.load(home_path + "/data/processed/ima.npy"))
     Y_ima_pred = tf.reshape(model.predict(X_ima), shape=(-1,))
 
     print(Y_ima_pred)
-    print("Ok")
 
 
 if __name__ == '__main__':
